564c174b8f8913ec04f9f2602dfc80474b65ca78
Thibaud <thibaud@DESKTOP-LSQI08G.hwxxjdfr>
2020-10-14 22:12:55 +0200

    No segfault, lets see if it work ...

--- XOR_Neural_Network/neural_network.c
+++ XOR_Neural_Network/neural_network.c
@@ -15,7 +15,7 @@ int main()
 	Fill_mat_data(expected_output,data_expected_output,4);
 
 	
-	long nb_rep = 10000;
+	long nb_rep = 100000;
 	double precision = 0.1;
 
 	int input_layer_neurons = 2;
@@ -34,9 +34,9 @@ int main()
 	Matrix output_bias = Init_matrix(output_layer_neurons,1);
 	Fill_mat_rand(output_bias);
 	
-	Matrix res_hidden_layer = Init_matrix(hidden_weight.nb_column,inputs.nb_rows);
+	Matrix res_hidden_layer = Init_matrix(hidden_weight.nb_column,input.nb_rows);
 		
-	Matrix res_hidden_layer = Init_matrix(res_hidden_layer.nb_column,res_hidden_layer.nb_rows);
+	//Matrix res_hidden_layer = Init_matrix(res_hidden_layer.nb_column,res_hidden_layer.nb_rows);
 
 	Matrix hidden_layer_output = Init_matrix(res_hidden_layer.nb_column,res_hidden_layer.nb_rows);
 	
@@ -52,6 +52,37 @@ int main()
 		
 	Matrix back_final_res = Init_matrix(error.nb_column,error.nb_rows);
 
+	Matrix error_hidden_layer_transp = Init_matrix(output_weight.nb_rows,output_weight.nb_column);
+
+	Matrix error_hidden_layer = Init_matrix(error_hidden_layer_transp.nb_column,back_final_res.nb_rows);
+
+	Matrix back_hidden_layer_sigmod = Init_matrix(hidden_layer_output.nb_column,hidden_layer_output.nb_rows);
+
+	Matrix back_hidden_layer = Init_matrix(error_hidden_layer.nb_column,error_hidden_layer.nb_rows);
+
+	Matrix output_weight_back_transp = Init_matrix(hidden_layer_output.nb_rows,hidden_layer_output.nb_column);
+
+	Matrix output_weight_back_multmath = Init_matrix(back_final_res.nb_column,output_weight_back_transp.nb_rows);
+
+	Matrix output_weight_back_multfac = Init_matrix(output_weight_back_multmath.nb_column,output_weight_back_multmath.nb_rows);
+	
+	Matrix output_bias_back_sumcol = Init_matrix(back_final_res.nb_column,1);
+
+	Matrix output_bias_back_multifac = Init_matrix(output_bias_back_sumcol.nb_column,output_bias_back_sumcol.nb_rows);
+
+	Matrix hidden_weight_back_transp = Init_matrix(input.nb_rows,input.nb_column);
+
+	Matrix hidden_weight_back_multmath = Init_matrix(back_hidden_layer.nb_column,hidden_weight_back_transp.nb_rows);
+		
+	Matrix hidden_weight_back_multfac = Init_matrix(hidden_weight_back_multmath.nb_column,hidden_weight_back_multmath.nb_rows);
+
+	Matrix hidden_bias_back_sumcol = Init_matrix(back_hidden_layer.nb_column,1);
+
+	Matrix hidden_bias_back_multfact = Init_matrix(hidden_bias_back_sumcol.nb_column,hidden_bias_back_sumcol.nb_rows);
+
+
+
+
 
 	for(;nb_rep > 0;--nb_rep)
 	{
@@ -66,13 +97,15 @@ int main()
 		
 		Mult_mat_1(hidden_layer_output,output_weight,res_output_layer);
 		
-		res_output_layer = Sum_bias(res_output_layer,output_bias);
+		Sum_bias(res_output_layer,output_bias,res_output_layer);
 		
 		Sigmo_mat(res_output_layer,final_res);
-		
+	
+		Pretty_print(final_res);	
 
 		//BACKPROPAGATION
 		Multip_factor(final_res,(-1),error_multip_factor);
+		
 		Sum_weights(error_multip_factor,expected_output,error);	
 
 
@@ -80,23 +113,51 @@ int main()
 		Sigmo_mat(final_res,back_final_res_sigmo);
 		Mult_simple(error,back_final_res_sigmo,back_final_res);
 
+		Mult_mat_1(back_final_res,error_hidden_layer_transp,error_hidden_layer);
+
+	
+		Sigmo_mat_derivate(hidden_layer_output,back_hidden_layer_sigmod);
+		Mult_simple(error_hidden_layer,back_hidden_layer_sigmod,back_hidden_layer);
 
 
 
-`		Matrix error_hidden_layer = Mult_mat_1(back_final_res,Transp_mat(output_weight));
 
-				
-		Matrix back_hidden_layer = Mult_simple(error_hidden_layer,Sigmo_mat_derivate(hidden_layer_output));
- 
-		output_weight = Sum_weights(output_weight,Multip_factor(Mult_mat_1(Transp_mat(hidden_layer_output),back_final_res),precision)); 
-		output_bias = Sum_bias(output_bias,Multip_factor(Sum_column(back_final_res),precision));
+		Transp_mat(hidden_layer_output,output_weight_back_transp);
+	
+
+		Mult_mat_1(output_weight_back_transp,back_final_res,output_weight_back_multmath);
 		
-		hidden_weight = Sum_weights(hidden_weight,Multip_factor(Mult_mat_1(Transp_mat(input),back_hidden_layer),precision));
+		Multip_factor(output_weight_back_multmath,precision,output_weight_back_multfac);
 		
-		hidden_bias = Sum_weights(hidden_bias,Multip_factor(Sum_column(back_hidden_layer),precision));
+		Sum_weights(output_weight,output_weight_back_multfac,output_weight);
+
+		
+
+
+		Sum_column(back_final_res,output_bias_back_sumcol);
+
+		Multip_factor(output_bias_back_sumcol,precision,output_bias_back_multifac);
+
+		Sum_bias(output_bias,output_bias_back_multifac,output_bias);
+
+
+
+		Transp_mat(input,hidden_weight_back_transp);
+
+		Mult_mat_1(hidden_weight_back_transp,back_hidden_layer,hidden_weight_back_multmath);
+
+		Multip_factor(hidden_weight_back_multmath,precision,hidden_weight_back_multfac);
+		Sum_weights(hidden_weight,hidden_weight_back_multfac,hidden_weight);
+
+	
+		
+		Sum_column(back_hidden_layer,hidden_bias_back_sumcol);
+
+		Multip_factor(hidden_bias_back_sumcol,precision,hidden_bias_back_multfact);
+		Sum_weights(hidden_bias,hidden_bias_back_multfact,hidden_bias);
 			
 	}	
-
+	
 	return 0;
 }
 
